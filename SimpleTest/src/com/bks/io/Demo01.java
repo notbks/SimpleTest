9d66abe0c31f1a8b53e0e67c568c863ac1b8cfd7
notbks <820290148@qq.com>
2017-11-27 13:54:39 +0800

    11月22号做的一些基础题

---
+++ SimpleTest/src/com/bks/io/Demo01.java
@@ -0,0 +1,14 @@
+package com.bks.io;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char c ='a';
+		char c2 =97;
+		System.out.println(c2==c);
+		
+		float f =200.222f;
+	}
+
+}
